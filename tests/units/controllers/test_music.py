4c71469612297c28e422cb874f1416593ea6e718
bnbong <bbbong9@gmail.com>
2023-02-15 04:16:34 +0000

    made controllers/music.py failure test cases

--- tests/units/controllers/test_music.py
+++ tests/units/controllers/test_music.py
@@ -1,8 +1,12 @@
 import pytest_asyncio
 import pytest
-import httpx
 
-from tests.helper import with_app_ctx, ensure_fresh_env
+from tests.helper import (
+    with_app_ctx,
+    ensure_fresh_env,
+    create_async_function,
+    FakeResponse,
+)
 from tests.mock.user import create_user
 from httpx import AsyncClient
 from _pytest.monkeypatch import MonkeyPatch
@@ -10,11 +14,12 @@ from _pytest.monkeypatch import MonkeyPatch
 from app.settings import AppSettings
 from app.utils import auth as auth_util
 from app.utils import spotify as spotify_util
+from app.utils import fastapi as fastapi_util
 from app.controllers import music as fastapi_music
 
 
 class TestMusic:
-    spfy_handler = spotify_util.SpotifyApiHandler() 
+    spfy_handler = spotify_util.SpotifyApiHandler()
 
     @pytest_asyncio.fixture(scope="class", autouse=True)
     async def _init_env(
@@ -25,7 +30,10 @@ class TestMusic:
             await create_user(app_client)
 
     @pytest_asyncio.fixture(scope="class")
-    async def spotify_access_token(self, app_settings: AppSettings,) -> str:
+    async def spotify_access_token(
+        self,
+        app_settings: AppSettings,
+    ) -> str:
         async with with_app_ctx(app_settings):
             spotify_access_token = await self.spfy_handler.client_credentials
 
@@ -38,7 +46,7 @@ class TestMusic:
     ) -> None:
         resp = await app_client.get(
             "/music/track",
-            params={"q": "별을 담은 시"},
+            params={"q": "abcdefu"},
             headers={"Authorization": "Bearer " + user_access_token},
         )
 
@@ -53,9 +61,11 @@ class TestMusic:
     ) -> None:
         headers = {
             "Authorization": "Bearer " + user_access_token,
-            "spotify_access_token": spotify_access_token
+            "spotify_access_token": spotify_access_token,
         }
-        resp = await app_client.get('/music/track/4fouWK6XVHhzl78KzQ1UjL', headers=headers)
+        resp = await app_client.get(
+            "/music/track/4fouWK6XVHhzl78KzQ1UjL", headers=headers
+        )
 
         assert resp.status_code == 200
         assert resp.json() is not None
@@ -68,12 +78,15 @@ class TestMusic:
     ) -> None:
         headers = {
             "Authorization": "Bearer " + user_access_token,
-            "spotify_access_token": spotify_access_token
+            "spotify_access_token": spotify_access_token,
         }
-        resp = await app_client.get('/music/artist/2VSHKHBTiXWplO8lxcnUC9', headers=headers)
+        resp = await app_client.get(
+            "/music/artist/2VSHKHBTiXWplO8lxcnUC9", headers=headers
+        )
 
         assert resp.status_code == 200
         assert resp.json() is not None
+
 
 class TestMusicFail:
     @pytest_asyncio.fixture(scope="class", autouse=True)
@@ -84,27 +97,127 @@ class TestMusicFail:
             await ensure_fresh_env()
             await create_user(app_client)
 
+    @pytest.mark.parametrize(
+        "expected_error_code, expected_error_message",
+        [("spotify_error", "spotify_error")],
+    )
     async def test_music_track_search_api_fail(
         self,
+        monkeypatch: MonkeyPatch,
+        app_settings: AppSettings,
+        expected_error_code: str | None,
+        expected_error_message: str | None,
     ) -> None:
-        pass
+        async with with_app_ctx(app_settings):
+            with monkeypatch.context() as mp:
+                mp.setattr(
+                    AsyncClient,
+                    "get",
+                    create_async_function(
+                        lambda *args, **kwargs: FakeResponse(
+                            data={"message": "something_error_happens_at_spotify"},
+                            status_code=409,
+                        )
+                    ),
+                )
+                q = {"q": "abcdefu"}
 
+                with pytest.raises(fastapi_util.LogicError) as err:
+                    await fastapi_music.music_track_search_api(q=q)
+
+        assert err.value.code == expected_error_code
+        assert err.value.message == expected_error_message
+
+    @pytest.mark.parametrize(
+        "expected_error_code, expected_error_message",
+        [("no_access_token", "you should send spotify access token")],
+    )
     async def test_music_track_get_api_fail_no_token(
         self,
+        app_client: AsyncClient,
+        user_access_token: str,
+        expected_error_code: str | None,
+        expected_error_message: str | None,
     ) -> None:
-        pass
+        headers = {
+            "Authorization": "Bearer " + user_access_token,
+        }
+        resp = await app_client.get(
+            "/music/track/4fouWK6XVHhzl78KzQ1UjL", headers=headers
+        )
 
+        assert (resp.json() or {}).get("detail", {}).get("code") == expected_error_code
+        assert (resp.json() or {}).get("detail", {}).get(
+            "message"
+        ) == expected_error_message
+
+    @pytest.mark.parametrize(
+        "expected_error_code, expected_error_message",
+        [("spotify_error", "spotify_error")],
+    )
     async def test_music_track_get_api_fail(
         self,
+        app_client: AsyncClient,
+        user_access_token: str,
+        expected_error_code: str | None,
+        expected_error_message: str | None,
     ) -> None:
-        pass
+        headers = {
+            "Authorization": "Bearer " + user_access_token,
+            "spotify_access_token": "wrong_spotify_token",
+        }
+        resp = await app_client.get(
+            "/music/track/4fouWK6XVHhzl78KzQ1UjL", headers=headers
+        )
 
+        assert (resp.json() or {}).get("detail", {}).get("code") == expected_error_code
+        assert (resp.json() or {}).get("detail", {}).get(
+            "message"
+        ) == expected_error_message
+
+    @pytest.mark.parametrize(
+        "expected_error_code, expected_error_message",
+        [("no_access_token", "you should send spotify access token")],
+    )
     async def test_music_artist_get_api_fail_no_token(
         self,
+        app_client: AsyncClient,
+        user_access_token: str,
+        expected_error_code: str | None,
+        expected_error_message: str | None,
     ) -> None:
-        pass
+        headers = {
+            "Authorization": "Bearer " + user_access_token,
+        }
+        resp = await app_client.get(
+            "/music/artist/2VSHKHBTiXWplO8lxcnUC9", headers=headers
+        )
 
+        assert (resp.json() or {}).get("detail", {}).get("code") == expected_error_code
+        assert (resp.json() or {}).get("detail", {}).get(
+            "message"
+        ) == expected_error_message
+
+    @pytest.mark.parametrize(
+        "expected_error_code, expected_error_message",
+        [("spotify_error", "spotify_error")],
+    )
     async def test_music_artist_get_api_fail(
         self,
+        app_client: AsyncClient,
+        user_access_token: str,
+        expected_error_code: str | None,
+        expected_error_message: str | None,
     ) -> None:
-        pass
+        headers = {
+            "Authorization": "Bearer " + user_access_token,
+            "spotify_access_token": "wrong_spotify_token",
+        }
+        resp = await app_client.get(
+            "/music/artist/2VSHKHBTiXWplO8lxcnUC9", headers=headers
+        )
+
+        assert (resp.json() or {}).get("detail", {}).get("code") == expected_error_code
+        assert (resp.json() or {}).get("detail", {}).get(
+            "message"
+        ) == expected_error_message
